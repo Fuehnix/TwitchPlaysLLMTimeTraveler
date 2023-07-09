c6563e1f59653094d8975c4c7d56f5605b412abe
Matthew D. Scholefield <matthew331199@gmail.com>
2023-07-08 21:04:28 -0700

    Setup basic project and cleanup code

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
-# TwitchPlaysLLMTimeTraveler
+# Twitch Plays Llm
 
-LTT (LLM Time Traveler) is a text-based choose-your-own-adventure game (ie. AI dungeon) set within a historic  time period. 
+*A collaborative twitch-based choose-your-own-adventure game*
 
-## Requirements
-[![Python Version](https://img.shields.io/badge/Python-3.10.11-blue.svg)](https://www.python.org/downloads/release/python-31011/)
+Twitch Plays Llm a text-based choose-your-own-adventure game (ie. AI dungeon) set within a specific theme. For the time travel theme, the main character is a timem traveler vising a historic time period with the goal of altering some historic event. The game is played collaboratively through twitch chat. Read more about the vision for this project within [this document](https://docs.google.com/document/d/10TJ-P2iRqNIOWyQ5PRzcVnN7VBCprzPSB9CFGy_-eDo/edit).
+
+## Development Setup
+
+This project is a standard Python package and can be installed like this:
+
+```bash
+python3 -m venv .venv/  # Setup a virtual environment
+source .venv/bin/activate  # Activate virtual environment
+pip install -e .  # Install the local folder as a Python package
+```
+
+Run the executable:
+```bash
+# Make sure to run before hand: .venv/bin/activate
+twitch-plays-llm -h  # View the available commands
+```
+
--- twitch_plays_llm/__main__.py
+++ twitch_plays_llm/__main__.py
@@ -4,6 +4,7 @@ import json
 import asyncio
 from pydantic import BaseModel
 
+
 # Load the configuration file
 with open('config.json') as config_file:
     config = json.load(config_file)
@@ -13,6 +14,7 @@ client_id = config['twitch']['clientkey']
 channel_name = config['twitch']['hostchannel']
 openai_api_key = config['openai']['api_key']
 openai.api_key = openai_api_key
+
 
 # BaseModel is similar to a dataclass (used to store data in a structure way)
 class StoryEntry(BaseModel):
@@ -29,8 +31,14 @@ class StoryGenerator:
                 narration_result="You are a middle aged man in downtown Chicago, 1910. You're in a steak restaurant talking to the waiter as you just sat down."
             )
         ]
-    
+
     def construct_prompt_messages(self, user_action: str):
+        # === ChatCompletions API reference ===
+        # system: tells ChatGPT what it's role is/the context of its responses
+        # assistant: pseudo-history of messages from openai model
+        # user: pseudo-history of messages from user
+        #
+        # Bot will then try to complete the conversation
         messages = [
             {"role": "system", "content": "You are a storyteller that generates the next event in a story based on the action a user says. The story takes place in 1910 and the main character is a middle aged man. At each turn, the user says an action and you reply with a short continuation of the story outlining the events that happen in the story based on the action the user performed."},
         ]
@@ -43,13 +51,9 @@ class StoryGenerator:
             {"role": "user", "content": user_action}
         )
         return messages
-        
-    #system: tells ChatGPT what it's role is/the context of its responses
-    #assistant: pseudo-history of messages from openai model
-    #user: pseudo-history of messages from user
-    #Bot will then try to complete the conversation
+
     def generate_next_story_narration(self, user_action: str):
-        """"""
+        """Generates the continuation of the story given a user action"""
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=self.construct_prompt_messages(user_action)
@@ -60,45 +64,58 @@ class StoryGenerator:
         )
         return next_narration
 
-generator = StoryGenerator()
-max_message_len = 500
 
 class Bot(commands.Bot):
+    max_message_len = 500  # Twitch has a 500 character limit
+
     def __init__(self):
         # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
         super().__init__(token=client_id, prefix='!', initial_channels=[channel_name])
+        self.generator = StoryGenerator()
 
     async def event_ready(self):
         """Function that runs when bot connects to server"""
         print(f'Logged in as | {self.nick}')
         print(f'User id is | {self.user_id}')
-        if generator.past_story_entries:
-            last_narration = generator.past_story_entries[-1].narration_result
+        if self.generator.past_story_entries:
+            last_narration = self.generator.past_story_entries[-1].narration_result
             await self.get_channel(channel_name).send(f'Story: {last_narration}')
-    
+
     @commands.command()
     async def action(self, ctx: commands.Context):
-        """Command to continue the story by performing an action"""
-        user_action = ctx.message.content.split(' ', 1)[1]
-        await ctx.send(f'Generating continuation of story...')
-        narration_result = generator.generate_next_story_narration(user_action)
-        while narration_result:
-            suffix = '...' if len(narration_result) >= max_message_len else ''
-            await ctx.send(narration_result[:max_message_len - 3] + suffix)
-            await asyncio.sleep(2)
-            narration_result = narration_result[max_message_len - 3:]
+        """Trigger for user to perofrm an action within the game"""
+        user_action = self._extract_message_text(ctx)
+        await self._perform_action(user_action, ctx)
 
     @commands.command()
     async def say(self, ctx: commands.Context):
-        """Command to continue the story by performing an action"""
-        user_action = "You say \"" + ctx.message.content.split(' ', 1)[1] + "\""
-        await ctx.send(f'Generating continuation of story...')
-        narration_result = generator.generate_next_story_narration(user_action)
-        while narration_result:
-            suffix = '...' if len(narration_result) >= max_message_len else ''
-            await ctx.send(narration_result[:max_message_len - 3] + suffix)
-            await asyncio.sleep(2)  # We need to sleep because otherwise twitch drops the message for some reason
-            narration_result = narration_result[max_message_len - 3:]
+        """Trigger for user to say something within the game"""
+        user_action = "You say \"" + self._extract_message_text(ctx) + "\""
+        await self._perform_action(user_action, ctx)
 
-bot = Bot()
-bot.run()
+    async def _perform_action(self, user_action: str, ctx: commands.Context):
+        """Continues the story by performing an action, communicating the result to the channel"""
+        await ctx.send('Generating continuation of story...')
+        narration_result = self.generator.generate_next_story_narration(user_action)
+        while narration_result:
+            suffix = '...' if len(narration_result) >= self.max_message_len else ''
+            await ctx.send(narration_result[:self.max_message_len - 3] + suffix)
+            await asyncio.sleep(2)
+            narration_result = narration_result[self.max_message_len - 3:]
+
+    @staticmethod
+    def _extract_message_text(ctx: commands.Context) -> str:
+        """
+        Extract the text part of a user message after the command
+        (ie. "bar baz" from the message "!foo bar baz")
+        """
+        return ctx.message.content.split(' ', 1)[1]
+
+
+def main():
+    bot = Bot()
+    bot.run()
+
+
+if __name__ == '__main__':
+    main()
