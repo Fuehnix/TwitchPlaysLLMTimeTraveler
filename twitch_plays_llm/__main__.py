fa7b03ab6e4a5da648f5c7c0733e2b217d47ac9a
Matthew D. Scholefield <matthew331199@gmail.com>
2023-07-10 23:55:32 -0700

    Add argument parsing

--- twitch_plays_llm/__main__.py
+++ twitch_plays_llm/__main__.py
@@ -1,3 +1,5 @@
+from argparse import ArgumentParser
+
 import openai
 
 from .config import config
@@ -6,11 +8,21 @@ from .llm_twitch_bot import LlmTwitchBot
 
 
 def main():
-    openai.api_key = config.openai_api_key
-    llm_game = LlmGame()
-    bot = LlmTwitchBot(llm_game)
-    llm_game.hooks = bot
-    bot.run()
+    parser = ArgumentParser(
+        description='Backend for Twitch-Plays-LLM, an interactive collaborative text-based twitch game'
+    )
+    sp = parser.add_subparsers(dest='action')
+    sp.add_parser('run')
+    args = parser.parse_args()
+
+    if args.action == 'run':
+        openai.api_key = config.openai_api_key
+        llm_game = LlmGame()
+        bot = LlmTwitchBot(llm_game)
+        llm_game.hooks = bot
+        bot.run()
+    else:
+        assert False
 
 
 if __name__ == '__main__':
