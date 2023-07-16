49ce63c7a84f1fe39fd7e809549c9e1c4474d123
Jacob Fuehne <jacobfuehne@yahoo.com>
2023-07-16 19:31:48 -0400

    Update story_generator.py

--- twitch_plays_llm/story_generator.py
+++ twitch_plays_llm/story_generator.py
@@ -14,48 +14,31 @@ class StoryGenerator:
                 # narration_result="You are a middle aged man in downtown Chicago, 1910. You're in a steak restaurant talking to the waiter as you just sat down.",
                 # narration_result="You are a quirky time travelling inventor with a handlebar mustache and a knack for mischievous inventions. Blinking your eyes open, you realize you have accidentally landed in the year 1875, right in the heart of a bustling Wild West town. Dusty roads, saloons, and cowboys on horseback surround you, while the sound of piano music drifts through the air.",
                 narration_result=
-                "In the heart of the soot-covered city of Cogspeak, where towering edifices of "
-                "brass and iron belch steam into the clouded sky, resides Esther, a blend of "
-                "artist, programmer, and aviary admirer. Her living quarters, an entanglement of "
-                "refurbished clockwork machinery and assorted avian-inspired artworks, oscillates "
-                "between the symphony of grinding gears and the harmonious chirping of caged "
-                "mechanical birds. Esther’s world is steeped in Steampunk charm, yet it brims "
-                "with the arcane; she wields the power to morph into any bird she desires, an "
-                "ability she often uses for swift travel across the sprawling metropolis or for "
-                "surreptitious spying on the city's upper echelons. In the maelstrom of her mind, "
-                "a multitude of voices echoes - a chorus she lovingly dubs her 'Twitch'. These "
-                "voices command her actions, guiding her through time-warping exploits that not "
-                "only defy the standard tick-tock flow of her world but also have a profound "
-                "impact on Cogspeak’s timeline, with every decision setting off a chain of events "
-                "that ripple through her reality. One balmy evening, as Esther was transforming "
-                "back to her human form after an exhilarating flight across Cogspeak, a "
-                "thunderous tremor rocked the city, followed by the eerie whine of a machine in "
-                "duress. When she looked out, she saw a pillar of ghostly green light shooting "
-                "skywards from the center of the city, a place notoriously known as the "
-                "Clocktower Citadel - the heart of time itself. In the aftermath, the rhythmic "
-                "ticking that underpinned life in Cogspeak fell silent, and time stood eerily "
-                "still. Soon, her 'Twitch' was overwhelmed with pleas for help from terrified "
-                "cityfolk frozen in time. Esther knew she had to restore the Citadel's Clockwork "
-                "Heart to set time in motion again. Guided by the cacophony of her Twitch, she "
-                "embarked on a perilous quest through a city locked in a timeless prison, "
-                "carrying the hope of every soul entrapped within Cogspeak."
-            )
+                """Welcome, brave Esther, to the sprawling city of Gearlock, a symphony of cogwheel and steam where airships drift through the sooty skies and giant gearworks define the horizon. Here, a bird's-eye view is a literal commodity you possess, becoming as sparrows or falcons at will. Time isn't a river but a swirling eddy for you, bendable and controllable. The diverse, lively chatter of your "Twitch" keeps your world kaleidoscopic, pushing and pulling you through the cacophonic rhythm of your dual existence. As you walk through the vibrant brass streets, your skilled eyes see the intricate beauty of life sketched in every corner. A sudden flutter of wings catches your attention; a mechanical messenger pigeon lands near you, a note gripped in its tiny metallic talons. A quick scan of the message, and it's clear: an urgent summons from the enigmatic Clockwork Guildmaster, a call to action that your many voices are eager to answer."""            )
         ]
 
     def construct_initial_prompt(self):
         """Not used
         construct initial prompt for story generation"""
-        rules = """Create a writing prompt to start an RPG text adventure game.  Adhere to the following rules:
-                    1. The story should take place in Baldur's Gate from Dungeons and Dragons' Forgotten Realms.
-                    2  You should describe the player's characteristics, where they are, what time period they are in, and what surrounds them.
-                    3. Keep it fun and allow people to be creative.
-                    4. Use the 2nd person perspective.
-                    5. The prompt should be only 3 - 5 sentences long."""
+        # rules = """Create a writing prompt to start an RPG text adventure game.  Adhere to the following rules:
+        #             1. The story should take place in Baldur's Gate from Dungeons and Dragons' Forgotten Realms.
+        #             2  You should describe the player's characteristics, where they are, what time period they are in, and what surrounds them.
+        #             3. Keep it fun and allow people to be creative.
+        #             4. Use the 2nd person perspective.
+        #             5. The prompt should be only 3 - 5 sentences long."""
+        rules = """Please complete the following story prompt by describing the start of a Steampunk setting for a text adventure game:
+                Please keep your response
+                1. limited to a paragraph 
+                2. write in the style of Critical Role D&D
+                3. Include a vague call to action for the player to act on, such as a messenger having a quest, or seeing someone in an alley, etc.
+
+                Story prompt: 
+                You are a human woman named Esther, a friendly bird obsessed artist and programmer. You have the power to transform into a bird and have time related abilities.  You have many voices in your head that control your actions, sometimes you call them your “Twitch”. """
         messages = [{ 'role': 'user',
                 'content': rules}]
 
         response = openai.ChatCompletion.create(
-            model='gpt-3.5-turbo',
+            model='gpt-3.5-turbo-16k',
             messages = messages,
         )
         initial_prompt = response['choices'][0]['message']['content']
@@ -85,7 +68,9 @@ class StoryGenerator:
 
                             1. If an action is unsuccessful, respond with a relevant consequence.
 
-                            2. Allow players to be creative, but nudge them towards the main quest. 
+                            2. Allow players to be creative, and indulge them in their distractions.
+
+                            3. The voices in Esther's head can compel her to do anything.
 
                             Refer back to these rules after every prompt.
 
@@ -109,7 +94,7 @@ class StoryGenerator:
     def generate_next_story_narration(self, story_action: str):
         """Generates the continuation of the story given a user action"""
         response = openai.ChatCompletion.create(
-            model='gpt-3.5-turbo',
+            model='gpt-3.5-turbo-16k',
             messages=self.construct_prompt_messages(story_action),
         )
         next_narration = response['choices'][0]['message']['content']
@@ -124,6 +109,7 @@ class StoryGenerator:
                 story_action='',
                 # narration_result="You are a middle aged man in downtown Chicago, 1910. You're in a steak restaurant talking to the waiter as you just sat down.",
                 # narration_result="You are a quirky time travelling inventor with a handlebar mustache and a knack for mischievous inventions. Blinking your eyes open, you realize you have accidentally landed in the year 1875, right in the heart of a bustling Wild West town. Dusty roads, saloons, and cowboys on horseback surround you, while the sound of piano music drifts through the air.",
-                narration_result="You're a jovial gnome bard, renowned across the taverns of Baldur's Gate for your quick wit and sparkling lute melodies. It's the Year of the Spitting Triton, and you find yourself in the bustling city market, surrounded by a cacophony of traders, minstrels, and townsfolk. Suddenly, a bedraggled messenger races towards you - he carries a message ",
+                # narration_result="""In the heart of the iron-clad city of Gearford, within the cloud-shrouded aeries of the Cog Tower, you, Esther, find solace among the thrumming machinations and whistling steam pipes, your fingers dancing across the canvas and keyboard alike. From the corner of your eye, you witness the blinking gears of your ornithopter clock, its rhythmic tick-tocking a constant reminder of your temporal prowess. Yet, the whispering voices in your mind, your loyal Twitch, sing in discordant harmony, guiding, prodding, or sometimes even commanding you. As you shape-shift into a shimmering bird and take flight, the metropolis sprawls beneath you, a mechanical marvel of brass and steam. Below, in the twisting alleyways, you catch sight of a frantic messenger being accosted by clockwork constables, his desperate eyes seemingly pleading for your intervention. It seems that Gearford, once again, requires the touch of your wing and the turn of your gear."""
+                narration_result= self.construct_initial_prompt(),
             )
         ]
